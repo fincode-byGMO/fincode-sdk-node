2f59153d53c8bfc5c72b4b9610ca28aa6429bd71
fincode-developer <fincode-developer@gmo-pg.com>
2023-03-16 14:12:27 +0900

    feat:node-fetch > payment session

--- src/api/v1/session.payment.ts
+++ src/api/v1/session.payment.ts
@@ -1,62 +1,75 @@
-// import { APIRawErrorResponse, CreatingPaymentSessionRequest, PaymentSessionObject, createUnknownError, formatErrorResponse } from "../../types/index.js"
-// import { FincodeConfig } from "./fincode.js"
-// import { createFincodeRequest } from "./http.js"
+import {
+    APIRawErrorResponse,
+    CreatingPaymentSessionRequest,
+    PaymentSessionObject,
+    createUnknownError,
+    formatErrorResponse
+} from "../../types/index.js"
+import { FincodeConfig } from "./fincode.js"
+import { createFincodeRequestFetch, FincodePartialRequestHeader } from "./http.js"
 
-// class PaymentSession {
+class PaymentSession {
 
-//     private readonly _config: FincodeConfig
+    private readonly _config: FincodeConfig
 
-//     constructor(config: FincodeConfig) {
-//         this._config = config
-//     }
+    constructor(config: FincodeConfig) {
+        this._config = config
+    }
 
-//     /**
-//      * **Create a card payment session**
-//      *
-//      * corresponds to `POST /v1/sessions`
-//      *
-//      * if the Promise is rejected, the error is an instance of `FincodeError`
-//      */
-//     public create(
-//         body: CreatingPaymentSessionRequest,
-//         header?: Parameters<typeof createFincodeRequest>[4]
-//     ): Promise<PaymentSessionObject> {
-//         return new Promise((resolve, reject) => {
-//             const req = createFincodeRequest(
-//                 this._config,
-//                 "POST",
-//                 `/v1/sessions`,
-//                 JSON.stringify(body),
-//                 header,
-//             )
+    /**
+     * **Create a payment session**
+     * 
+     * corresponds to `POST /v1/sessions`
+     * 
+     * if the Promise is rejected, the error is an instance of `FincodeError`
+     * 
+     * @param {RegisteringPaymentRequest} body
+     * @param {FincodePartialRequestHeader} [header]
+     * 
+     * @returns {Promise<PaymentSessionObject>}
+     */
+    public create(
+        body: CreatingPaymentSessionRequest,
+        header?: FincodePartialRequestHeader
+    ): Promise<PaymentSessionObject> {
+        return new Promise((resolve, reject) => {
+            const fetch = createFincodeRequestFetch(
+                this._config,
+                "POST",
+                "/v1/sessions",
+                JSON.stringify(body),
+                header,
+                {},
+            )
 
-//             req.on("response", res => {
-//                 const body: string[] = []
-//                 res.on("data", chunk => {
-//                     body.push(chunk)
-//                 })
-//                 res.on("end", () => {
-//                     const json = JSON.parse(body.join(""))
-//                     if (res.statusCode === 200) {
-//                         const session = json as PaymentSessionObject
+            fetch().then((res) => {
+                if (res.ok) {
+                    res.json().then((json) => {
+                        const payment = json as PaymentSessionObject
+                        resolve(payment)
+                    }).catch((e) => {
+                        const message = (e instanceof Error) ? e.message : undefined
+                        const err = createUnknownError(message)
+                        reject(err)
+                    })
+                } else {
+                    res.json().then((json) => {
+                        const errRes = json as APIRawErrorResponse
+                        const err = formatErrorResponse(errRes)
+                        reject(err)
+                    }).catch((e) => {
+                        const message = (e instanceof Error) ? e.message : undefined
+                        const err = createUnknownError(message)
+                        reject(err)
+                    })
+                }
+            }).catch((e) => {
+                const message = (e instanceof Error) ? e.message : undefined
+                const err = createUnknownError(message)
+                reject(err)
+            })
+        })
+    }
+}
 
-//                         resolve(session)
-//                     } else {
-//                         try {
-//                             const errRes = json as APIRawErrorResponse
-//                             const err = formatErrorResponse(errRes)
-//                             reject(err)
-//                         } catch (e) {
-//                             const message = (e instanceof Error) ? e.message : undefined
-//                             const err = createUnknownError(message)
-//                             reject(err)
-//                         }
-//                     }
-//                 })
-//             })
-//             req.end()
-//         })
-//     }
-
-// }
-// export { PaymentSession }
+export { PaymentSession }
