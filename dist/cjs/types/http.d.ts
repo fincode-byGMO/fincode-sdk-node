78e7f48dbbfbe75bcccfd493d6e16a9f6bb747be
*TI82IVbP2m <nakatani@epsilon.jp>
2024-02-15 10:09:54 +0900

    fix: exclude dist directory from version control

--- dist/cjs/types/http.d.ts
+++
@@ -1,8 +0,0 @@
-export type FincodeRequestHeader = Record<string, string>;
-export declare const createFincodeRequestHeader: (params?: {
-    apiVersion?: string;
-    authorization?: string;
-    idempotentKey?: string;
-    tenantShopId?: string;
-    contentType?: string;
-}) => FincodeRequestHeader;
